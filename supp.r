a639e437ffd06788455cffa8964b5d2c66c63d19
Flora Le <flora.le@monash.edu>
2026-01-21 17:28:33 +1100

    tidy up

--- supp.r
+++ supp.r
@@ -6,14 +6,9 @@ m_tib_isi1 <- readRDS(file.path(out, "m_tib_isi1.rds"))
 
 pred_tibq5_isi1_sum <- readRDS(file.path(out, "pred_tibq5_isi1_sum.rds"))
 
-pred_tibq5_isi1_age_u45_sum <- readRDS(file.path(out, "pred_tibq5_isi1_age_u45_sum.rds"))
-pred_tibq5_isi1_age_45u_sum <- readRDS(file.path(out, "pred_tibq5_isi1_age_45u_sum.rds"))
-
-pred_tibq5_isi1_female_sum <- readRDS(file.path(out, "pred_tibq5_isi1_female_sum.rds"))
-pred_tibq5_isi1_male_sum <- readRDS(file.path(out, "pred_tibq5_isi1_male_sum.rds"))
-
-pred_tibq5_isi1_serv_home_sum <- readRDS(file.path(out, "pred_tibq5_isi1_serv_home_sum.rds"))
-pred_tibq5_isi1_serv_lab_sum <- readRDS(file.path(out, "pred_tibq5_isi1_serv_lab_sum.rds"))
+pred_tibq5_isi1_age_sum <- readRDS(file.path(out, "pred_tibq5_isi1_age_sum.rds"))
+pred_tibq5_isi1_sex_sum <- readRDS(file.path(out, "pred_tibq5_isi1_sex_sum.rds"))
+pred_tibq5_isi1_serv_sum <- readRDS(file.path(out, "pred_tibq5_isi1_serv_sum.rds"))
 
 plot_min_perc_isi1 <- pdftools::pdf_convert(file.path(out, "plot_min_perc_isi1.pdf"), filenames = file.path(out, "plot_min_perc_isi1.png"), dpi = 300)
 plot_age_min_perc_isi1 <- pdftools::pdf_convert(file.path(out, "plot_age_min_perc_isi1.pdf"), filenames = file.path(out, "plot_age_min_perc_isi1.png"), dpi = 300)
@@ -22,99 +17,96 @@ plot_serv_min_perc_isi1 <- pdftools::pdf_convert(file.path(out, "plot_serv_min_p
 
 ## Estimated difference by sleep opportunity
 ### adj -------------------------------------
-pred_tibq5_isi1_sum[, `Sleep Opportunity` := fifelse(
- grepl("q1_q234", par), "Short - Medium",
+pred_tibq5_isi1_sum[, `Sleep Opportunity Contrast` := fifelse(
+ grepl("q1_q234", par), "Short vs Medium",
  fifelse(
-  grepl("q5_q234", par), "Long - Medium",
-  fifelse(grepl("q1_q5", par), "Short - Long", NA_character_)
+  grepl("q5_q234", par), "Long vs Medium",
+  fifelse(grepl("q1_q5", par), "Short vs Long", NA_character_)
  )
 )]
-pred_tibq5_isi1_sum[, `Sleep Opportunity` := factor(`Sleep Opportunity`, levels = c("Short - Medium", "Long - Medium", "Short - Long"))]
+pred_tibq5_isi1_sum[, `Sleep Opportunity Contrast` := factor(`Sleep Opportunity Contrast`, levels = c("Short vs Medium", "Long vs Medium", "Short vs Long"))]
 pred_tibq5_isi1_sum[, `Sleep-Wake Part` := factor(part_label, levels = c("WD", "SOL", "WASO", "N1+2", "N3", "REM"))]
 pred_tibq5_isi1_sum[, `Estimated Difference` := fifelse(!is.na(est_min), est_min, est_perc)]
 pred_tibq5_isi1_sum[, In := fifelse(!is.na(est_min), "Minutes", "Percentage")]
 pred_tibq5_isi1_sum[, Sig := ifelse(!between(0, CI_low, CI_high), "$\\ast$", "")]
 
-sup_tibq5_isi1_sum <- pred_tibq5_isi1_sum[grepl("q1_q234|q5_q234|q1_q5", par)][, .(`Sleep Opportunity`, `Sleep-Wake Part`, `Estimated Difference`, In, Sig)]
-sup_tibq5_isi1_sum <- sup_tibq5_isi1_sum[order( In, `Sleep Opportunity`, `Sleep-Wake Part`, Sig)]
+sup_tibq5_isi1_sum <- pred_tibq5_isi1_sum[grepl("q1_q234|q5_q234|q1_q5", par)][, .(`Sleep Opportunity Contrast`, `Sleep-Wake Part`, `Estimated Difference`, In, Sig)]
+sup_tibq5_isi1_sum <- sup_tibq5_isi1_sum[order( In, `Sleep Opportunity Contrast`, `Sleep-Wake Part`, Sig)]
 sup_tibq5_isi1_sum <- sup_tibq5_isi1_sum[!is.na(`Sleep-Wake Part`)]
 
 ### age -------------------------------------
-pred_tibq5_isi1_age_u45_sum[, age := "u45"]
-pred_tibq5_isi1_age_45u_sum[, age := "45u"]
-colnames(pred_tibq5_isi1_age_u45_sum)
-colnames(pred_tibq5_isi1_age_45u_sum)
-
-pred_tibq5_isi1_age_sum <- rbind(
-  pred_tibq5_isi1_age_u45_sum,
-  pred_tibq5_isi1_age_45u_sum
-)
-pred_tibq5_isi1_age_sum[, `Sleep Opportunity` := fifelse(
- grepl("q1_q234", par), "Short - Medium",
+pred_tibq5_isi1_age_sum[, `Sleep Opportunity Contrast` := fifelse(
+ grepl("q1_q234", par), "Short vs Medium",
  fifelse(
-  grepl("q5_q234", par), "Long - Medium",
-  fifelse(grepl("q1_q5", par), "Short - Long", NA_character_)
+  grepl("q5_q234", par), "Long vs Medium",
+  fifelse(grepl("q1_q5", par), "Short vs Long", NA_character_)
  )
 )]
-pred_tibq5_isi1_age_sum[, `Sleep Opportunity` := factor(`Sleep Opportunity`, levels = c("Short - Medium", "Long - Medium", "Short - Long"))]
+
+pred_tibq5_isi1_age_sum[, `Sleep Opportunity Contrast` := factor(`Sleep Opportunity Contrast`, levels = c("Short vs Medium", "Long vs Medium", "Short vs Long"))]
 pred_tibq5_isi1_age_sum[, `Sleep-Wake Part` := factor(part_label, levels = c("WD", "SOL", "WASO", "N1+2", "N3", "REM"))]
 pred_tibq5_isi1_age_sum[, `Estimated Difference` := fifelse(!is.na(est_min), est_min, est_perc)]
 pred_tibq5_isi1_age_sum[, In := fifelse(!is.na(est_min), "Minutes", "Percentage")]
 pred_tibq5_isi1_age_sum[, Age := fifelse(age == "u45", "Below 45y", "45y and above")]
 pred_tibq5_isi1_age_sum[, Sig := ifelse(!between(0, CI_low, CI_high), "$\\ast$", "")]
+pred_tibq5_isi1_age_sum[, `Age Contrast` := "45y and above vs Below 45y"]
+pred_tibq5_isi1_age_sum[, `Sleep Opportunity Percentile` := tib_group]
+pred_tibq5_isi1_age_sum[, `Sleep Opportunity Percentile` := ifelse(!is.na(tib_group), tib_group, "Overall")]
 
-sup_tibq5_isi1_age_sum <- pred_tibq5_isi1_age_sum[grepl("q1_q234|q5_q234|q1_q5", par)][, .(Age, `Sleep Opportunity`, `Sleep-Wake Part`, `Estimated Difference`, In, Sig)]
-sup_tibq5_isi1_age_sum <- sup_tibq5_isi1_age_sum[order( In, `Sleep Opportunity`, `Sleep-Wake Part`, Age, Sig)]
+sup_tibq5_isi1_age_sum <- pred_tibq5_isi1_age_sum[grepl("q1_q234|q5_q234|q1_q5", par)][, .(Age, `Sleep-Wake Part`, `Sleep Opportunity Contrast`, `Estimated Difference`, In, Sig)]
+sup_tibq5_isi1_age_sum <- sup_tibq5_isi1_age_sum[order( In, `Sleep Opportunity Contrast`, `Sleep-Wake Part`)]
 sup_tibq5_isi1_age_sum <- sup_tibq5_isi1_age_sum[!is.na(`Sleep-Wake Part`)]
 
-### sex -------------------------------------
-pred_tibq5_isi1_female_sum[, sex := "Female"]
-pred_tibq5_isi1_male_sum[, sex := "Male"]
+sup_tibq5_isi1_age_dif <- pred_tibq5_isi1_age_sum[grepl("_45u_u45", par)][, .(`Age Contrast`, `Sleep-Wake Part`, `Sleep Opportunity Percentile`, `Estimated Difference`, In, Sig)]
+sup_tibq5_isi1_age_dif <- sup_tibq5_isi1_age_dif[order( In, `Age Contrast`, `Sleep-Wake Part`)]
+sup_tibq5_isi1_age_dif <- sup_tibq5_isi1_age_dif[!is.na(`Sleep-Wake Part`)]
 
-pred_tibq5_isi1_sex_sum <- rbind(
-  pred_tibq5_isi1_female_sum,
-  pred_tibq5_isi1_male_sum
-)
-pred_tibq5_isi1_sex_sum[, `Sleep Opportunity` := fifelse(
-  grepl("q1_q234", par), "Short - Medium",
+### sex -------------------------------------
+pred_tibq5_isi1_sex_sum[, `Sleep Opportunity Contrast` := fifelse(
+  grepl("q1_q234", par), "Short vs Medium",
   fifelse(
-    grepl("q5_q234", par), "Long - Medium",
-    fifelse(grepl("q1_q5", par), "Short - Long", NA_character_)
+    grepl("q5_q234", par), "Long vs Medium",
+    fifelse(grepl("q1_q5", par), "Short vs Long", NA_character_)
   )
 )]
-pred_tibq5_isi1_sex_sum[, `Sleep Opportunity` := factor(`Sleep Opportunity`, levels = c("Short - Medium", "Long - Medium", "Short - Long"))]
+pred_tibq5_isi1_sex_sum[, `Sleep Opportunity Contrast` := factor(`Sleep Opportunity Contrast`, levels = c("Short vs Medium", "Long vs Medium", "Short vs Long"))]
 pred_tibq5_isi1_sex_sum[, `Sleep-Wake Part` := factor(part_label, levels = c("WD", "SOL", "WASO", "N1+2", "N3", "REM"))]
 pred_tibq5_isi1_sex_sum[, `Estimated Difference` := fifelse(!is.na(est_min), est_min, est_perc)]
 pred_tibq5_isi1_sex_sum[, In := fifelse(!is.na(est_min), "Minutes", "Percentage")]
 pred_tibq5_isi1_sex_sum[, Sex := sex]
 pred_tibq5_isi1_sex_sum[, Sig := ifelse(!between(0, CI_low, CI_high), "$\\ast$", "")]
+pred_tibq5_isi1_sex_sum[, `Sex Contrast` := "Female vs Male"]
+pred_tibq5_isi1_sex_sum[, `Sleep Opportunity Percentile` := ifelse(!is.na(tib_group), tib_group, "Overall")]
 
-sup_tibq5_isi1_sex_sum <- pred_tibq5_isi1_sex_sum[grepl("q1_q234|q5_q234|q1_q5", par)][, .(Sex, `Sleep Opportunity`, `Sleep-Wake Part`, `Estimated Difference`, In, Sig)]
-sup_tibq5_isi1_sex_sum <- sup_tibq5_isi1_sex_sum[order(In, `Sleep Opportunity`, `Sleep-Wake Part`, Sex, Sig)]
+sup_tibq5_isi1_sex_sum <- pred_tibq5_isi1_sex_sum[grepl("q1_q234|q5_q234|q1_q5", par)][, .(Sex, `Sleep-Wake Part`, `Sleep Opportunity Contrast`, `Estimated Difference`, In, Sig)]
+sup_tibq5_isi1_sex_sum <- sup_tibq5_isi1_sex_sum[order( In, `Sleep Opportunity Contrast`, `Sleep-Wake Part`)]
 sup_tibq5_isi1_sex_sum <- sup_tibq5_isi1_sex_sum[!is.na(`Sleep-Wake Part`)]
 
+sup_tibq5_isi1_sex_dif <- pred_tibq5_isi1_sex_sum[grepl("female_male", par)][, .(`Sex Contrast`, `Sleep-Wake Part`, `Sleep Opportunity Percentile`, `Estimated Difference`, In, Sig)]
+sup_tibq5_isi1_sex_dif <- sup_tibq5_isi1_sex_dif[order( In, `Sex Contrast`, `Sleep-Wake Part`)]
+sup_tibq5_isi1_sex_dif <- sup_tibq5_isi1_sex_dif[!is.na(`Sleep-Wake Part`)]
+sup_tibq5_isi1_sex_dif[, ]
 ### service -------------------------------------
-pred_tibq5_isi1_serv_home_sum[, service := "Home"]
-pred_tibq5_isi1_serv_lab_sum[, service := "Lab"]
-
-pred_tibq5_isi1_serv_sum <- rbind(
-  pred_tibq5_isi1_serv_home_sum,
-  pred_tibq5_isi1_serv_lab_sum
-)
-pred_tibq5_isi1_serv_sum[, `Sleep Opportunity` := fifelse(
-  grepl("q1_q234", par), "Short - Medium",
+pred_tibq5_isi1_serv_sum[, `Sleep Opportunity Contrast` := fifelse(
+  grepl("q1_q234", par), "Short vs Medium",
   fifelse(
-    grepl("q5_q234", par), "Long - Medium",
-    fifelse(grepl("q1_q5", par), "Short - Long", NA_character_)
+    grepl("q5_q234", par), "Long vs Medium",
+    fifelse(grepl("q1_q5", par), "Short vs Long", NA_character_)
   )
 )]
-pred_tibq5_isi1_serv_sum[, `Sleep Opportunity` := factor(`Sleep Opportunity`, levels = c("Short - Medium", "Long - Medium", "Short - Long"))]
+pred_tibq5_isi1_serv_sum[, `Sleep Opportunity Contrast` := factor(`Sleep Opportunity Contrast`, levels = c("Short vs Medium", "Long vs Medium", "Short vs Long"))]
 pred_tibq5_isi1_serv_sum[, `Sleep-Wake Part` := factor(part_label, levels = c("WD", "SOL", "WASO", "N1+2", "N3", "REM"))]
 pred_tibq5_isi1_serv_sum[, `Estimated Difference` := fifelse(!is.na(est_min), est_min, est_perc)]
 pred_tibq5_isi1_serv_sum[, In := fifelse(!is.na(est_min), "Minutes", "Percentage")]
 pred_tibq5_isi1_serv_sum[, Service := fifelse(service == "Home", "Home PSG", "Lab PSG")]
 pred_tibq5_isi1_serv_sum[, Sig := ifelse(!between(0, CI_low, CI_high), "$\\ast$", "")]
+pred_tibq5_isi1_serv_sum[, `Service Contrast` := "Lab PSG vs Home PSG"]
+pred_tibq5_isi1_serv_sum[, `Sleep Opportunity Percentile` := ifelse(!is.na(tib_group), tib_group, "Overall")]
 
-sup_tibq5_isi1_serv_sum <- pred_tibq5_isi1_serv_sum[grepl("q1_q234|q5_q234|q1_q5", par)][, .(Service, `Sleep Opportunity`, `Sleep-Wake Part`, `Estimated Difference`, In, Sig)]
-sup_tibq5_isi1_serv_sum <- sup_tibq5_isi1_serv_sum[order(In, `Sleep Opportunity`, Service, `Sleep-Wake Part`, Sig)]
+sup_tibq5_isi1_serv_sum <- pred_tibq5_isi1_serv_sum[grepl("q1_q234|q5_q234|q1_q5", par)][, .(Service, `Sleep-Wake Part`, `Sleep Opportunity Contrast`, `Estimated Difference`, In, Sig)]
+sup_tibq5_isi1_serv_sum <- sup_tibq5_isi1_serv_sum[order( In, `Sleep Opportunity Contrast`, `Sleep-Wake Part`)]
 sup_tibq5_isi1_serv_sum <- sup_tibq5_isi1_serv_sum[!is.na(`Sleep-Wake Part`)]
+
+sup_tibq5_isi1_serv_dif <- pred_tibq5_isi1_serv_sum[grepl("lab_home", par)][, .(`Service Contrast`, `Sleep-Wake Part`, `Sleep Opportunity Percentile`, `Estimated Difference`, In, Sig)]
+sup_tibq5_isi1_serv_dif <- sup_tibq5_isi1_serv_dif[order( In, `Service Contrast`, `Sleep-Wake Part`)]
+sup_tibq5_isi1_serv_dif <- sup_tibq5_isi1_serv_dif[!is.na(`Sleep-Wake Part`)]
